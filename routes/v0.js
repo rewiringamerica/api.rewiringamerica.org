7f9408ad382e12688a084fb66e92b992476b34bc
Tom Carden <tomc@rewiringamerica.org>
2023-01-27 13:32:03 -0700

    add /incentives endpoint to v0

--- routes/v0.js
+++ routes/v0.js
@@ -1,5 +1,6 @@
 import calculateIncentives from '../lib/incentives-calculation.js';
 import fetchAMIsForZip from '../lib/fetch-amis-for-zip.js';
+import incentives from '../data/ira_incentives.json' assert { type: 'json' };
 
 const CalculatorSchema = {
   "description": "How much money will you get with the Inflation Reduction Act?",
@@ -10,6 +11,29 @@ const CalculatorSchema = {
     "200": {
       "description": "Successful response",
       "$ref": "WebsiteCalculatorResponse"
+    },
+    "400": {
+      "description": "Bad request",
+      "$ref": "Error"
+    }
+  }
+};
+
+const IncentivesSchema = {
+  "description": "What are all the incentives from the Inflation Reduction Act?",
+  "response": {
+    "200": {
+      "description": "Successful response",
+      "type": "object",
+      "required": ["incentives"],
+      "properties": {
+        "incentives": {
+          "type": "array",
+          "items": {
+            "$ref": "WebsiteIncentive"
+          }
+        }
+      }
     },
     "400": {
       "description": "Bad request",
@@ -31,5 +55,11 @@ export default async function (fastify, opts) {
     reply.status(200)
       .type('application/json')
       .send(result);
+  });
+
+  fastify.get("/api/v0/incentives", { schema: IncentivesSchema }, async (request, reply) => {
+    reply.status(200)
+      .type('application/json')
+      .send({ incentives });
   });
 }
