2ba1d255246902f51157b8898929ed0527686e04
Tom Carden <tomc@rewiringamerica.org>
2023-02-07 13:40:02 -0700

    clarify if/else and usage of constants

--- routes/v0.js
+++ routes/v0.js
@@ -1,7 +1,7 @@
 import calculateIncentives from '../lib/incentives-calculation.js';
 import fetchAMIsForZip from '../lib/fetch-amis-for-zip.js';
 import incentives from '../data/ira_incentives.json' assert { type: 'json' };
-import iraStateSavings from '../data/ira_state_savings.json' assert { type: 'json' };
+import IRA_STATE_SAVINGS from '../data/ira_state_savings.json' assert { type: 'json' };
 
 const CalculatorSchema = {
   "description": "How much money will you get with the Inflation Reduction Act?",
@@ -47,6 +47,7 @@ export default async function (fastify, opts) {
   fastify.get("/api/v0/calculator", { schema: CalculatorSchema }, async (request, reply) => {
     // TODO: refactor as a plugin like fastify.amis.getForZip(zip)?
     const amisForZip = await fetchAMIsForZip(fastify.sqlite, request.query.zip);
+
     const result = calculateIncentives(
       amisForZip,
       {
@@ -56,7 +57,7 @@ export default async function (fastify, opts) {
 
     // Website Calculator-only:
     // Annual savings from pregenerated model
-    const estimatedStateSavings = iraStateSavings[amisForZip?.location?.state_id].estimated_savings_heat_pump_ev;
+    const estimatedStateSavings = IRA_STATE_SAVINGS[amisForZip?.location?.state_id].estimated_savings_heat_pump_ev;
 
     result.estimated_annual_savings = estimatedStateSavings;
 
