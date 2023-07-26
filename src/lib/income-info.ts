1372c93c48902671945cb40097359ca77511d0d8
Owen Yamauchi <owen@rewiringamerica.org>
2023-07-26 18:17:15 -0400

    TypeScript for the fetch-AMIs code
    In the process of doing this I believe I've found a bug in which we're
    effectively treating every tract as non-urban. Filed here:
    https://app.asana.com/0/1205057814651000/1205147156077606

---
+++ src/lib/income-info.ts
@@ -0,0 +1,36 @@
+/**
+ * Corresponds to the "zips" table in sqlite. There are other columns, but
+ * state_id is the only one we need.
+ */
+export type ZipInfo = {
+  state_id: string;
+};
+
+/**
+ * Corresponds to the "ami" table in sqlite. There are a bunch of columns that
+ * we don't need to look at, and some that we look at using constructed keys
+ * (e.g. `l80_${household_size}`).
+ */
+export type AMI = {
+  metro: string;
+  median2022: string;
+  [colName: string]: string | number;
+};
+
+/**
+ * Corresponds to an aggregate query on the "tracts" table in sqlite.
+ */
+export type MFI = {
+  // This is either "true" or "false". TODO: sketchy to be doing MAX() on that
+  isUrban: string;
+  lowestMFI: number;
+  highestMFI: number;
+  lowestPovertyRate: number;
+  highestPovertyRate: number;
+};
+
+export type IncomeInfo = {
+  location: ZipInfo;
+  ami: AMI;
+  calculations: MFI;
+};
