d5473c7d7f375f29ad22a02b8af98ec8ac26f76c
Owen Yamauchi <owen@rewiringamerica.org>
2023-11-13 12:23:40 -0500

    restore short_description to v0

--- src/schemas/v0/incentive.ts
+++ src/schemas/v0/incentive.ts
@@ -133,6 +133,12 @@ export const WEBSITE_INCENTIVE_SCHEMA = {
     eligible: {
       type: 'boolean',
     },
+    short_description: {
+      type: 'string',
+      examples: [
+        'A 150 character (or shorter) display description for the incentive.',
+      ],
+    },
   },
   additionalProperties: false,
   examples: [
