e0c1d38fea8f278fbac5b9fb531c48fdb6353995
Maria Bajzek <mariab@rewiringamerica.org>
2023-11-28 16:00:18 -0500

    Remove unnecessary code from tests.

--- test/lib/incentive-relationships.test.ts
+++ test/lib/incentive-relationships.test.ts
@@ -1,22 +1,9 @@
-import { open } from 'sqlite';
-import sqlite3 from 'sqlite3';
-import { afterEach, beforeEach, test } from 'tap';
+import { test } from 'tap';
 import { AuthorityType } from '../../src/data/authorities';
 import { FilingStatus } from '../../src/data/tax_brackets';
 import { OwnerStatus } from '../../src/data/types/owner-status';
 import { calculateStateIncentivesAndSavings } from '../../src/lib/state-incentives-calculation';
 import { TEST_INCENTIVES } from '../mocks/state-incentives';
-
-beforeEach(async t => {
-  t.context.db = await open({
-    filename: './incentives-api.db',
-    driver: sqlite3.Database,
-  });
-});
-
-afterEach(async t => {
-  await t.context.db.close();
-});
 
 // This is a basic test to set up supplying test data to the calculator logic.
 // When the incentive relationship logic is implemented, we'll use this to
