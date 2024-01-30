80899a7c22cfa78de5c4019c19debd5da3208f2c
Monique de Zahr <mmezher@google.com>
2024-01-30 18:16:42 +0000

    Add supported columns, add alphabetization of authorities.

--- scripts/incentive-spreadsheet-registry.ts
+++ scripts/incentive-spreadsheet-registry.ts
@@ -34,4 +34,11 @@ export const FILES: { [ident: string]: IncentiveFile } = {
     enHeader: 'Program Description (style guide)',
     esHeader: 'Program Description (Spanish)',
   },
+  AZ: {
+    filepath: 'data/AZ/incentives.json',
+    sheetUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQEGP5ZcMknLdHAqBAeCUtdkNPtS0CiFzQzoM4bdbLWYqC_30j1lHLeJhMSKElFRuwRdrgcd46Gl54j/pub?gid=995688950&single=true&output=csv',
+    headerRowNumber: 2,
+    idHeader: 'ID',
+    enHeader: 'Program Description (style guide)',
+  },
 };
