3a632e6ee23716ee8f0733f9ddc8182efb3c9c3d
Monique de Zahr <mmezher@google.com>
2024-01-31 22:16:03 +0000

    Add types to allow for sorting, add test.

--- scripts/lib/authority-and-program-updater.ts
+++ scripts/lib/authority-and-program-updater.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import { Project } from 'ts-morph';
-
 const project = new Project({
   tsConfigFilePath: 'tsconfig.json',
 });
@@ -74,13 +73,17 @@ export function createProgramName(
   );
 }
 
-export function sortJsonAlphabeticallyByStateKey(json: StateToAuthorityMap) {
-  const ordered = Object.keys(json).sort().reduce((obj, key) => {
-    obj[key] = json[key];
-    return obj;
-  },  {} as StateToAuthorityMap);
+export function sortJsonAlphabeticallyByStateKey(
+  json: StateToAuthorityTypeMap,
+) {
+  const ordered = Object.keys(json)
+    .sort()
+    .reduce((obj, key) => {
+      obj[key] = json[key];
+      return obj;
+    }, {} as StateToAuthorityTypeMap);
   return ordered;
-  }
+}
 
 type AuthorityKey = string;
 type ProgramKey = string;
@@ -99,8 +102,16 @@ type AuthorityMap = {
   [index: AuthorityKey]: Authority;
 };
 
-type StateToAuthorityMap = {
-  [index: StateKey]: AuthorityMap;
+// These types encapsulate how we parse and then re-write JSON to the data/authorities.json file.
+type JsonAuthorities = {
+  [index: AuthorityKey]: { name: string };
+};
+type AuthorityType = string;
+type AuthorityTypeMap = {
+  [index: AuthorityType]: JsonAuthorities;
+};
+export type StateToAuthorityTypeMap = {
+  [index: StateKey]: AuthorityTypeMap;
 };
 
 export class AuthorityAndProgramUpdater {
@@ -149,7 +160,9 @@ export class AuthorityAndProgramUpdater {
   }
 
   updateAuthoritiesJson() {
-    const json = JSON.parse(fs.readFileSync(AUTHORITIES_JSON_FILE, 'utf-8'));
+    const json: StateToAuthorityTypeMap = JSON.parse(
+      fs.readFileSync(AUTHORITIES_JSON_FILE, 'utf-8'),
+    );
     const stateUpper = this.state.toUpperCase();
     if (stateUpper in json) {
       throw new Error(
@@ -178,7 +191,6 @@ export class AuthorityAndProgramUpdater {
       JSON.stringify(ordered_json, null, 2),
       'utf-8',
     );
-   
   }
 
   updateProgramsTs() {
