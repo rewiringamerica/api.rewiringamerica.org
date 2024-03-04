efb2c6345b2fba499a071edc52dde85834b0ab44
Dan Turcza <datu925@gmail.com>
2024-03-04 14:18:12 -0600

    Proof of concept for merging programs.ts and programs.json.
    Remove programs.json.
    
    Split programs by state.
    
    Try to get back type safety.
    
    Add DC.
    
    Add as const to stragglers.
    
    Move programs ts files into src to avoid docker issues.

---
+++ src/data/programs/ga_programs.ts
@@ -0,0 +1,10 @@
+export const GA_PROGRAMS = {
+  ga_georgiaPower_homeEnergyImprovementProgram: {
+    name: {
+      en: 'Home Energy Improvement Program',
+    },
+    url: {
+      en: 'https://www.georgiapower.com/residential/save-money-and-energy/products-programs/home-energy-efficiency-programs/home-energy-improvementprogram.html',
+    },
+  },
+} as const;
