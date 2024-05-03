0b92f0abedd85d1ddfec4400a61376b9ff743acf
Owen Yamauchi <owen@rewiringamerica.org>
2024-05-03 12:41:12 -0400

    Get check-spreadsheet-health passing again
    ## Description
    
    The DC incentives just got reordered for some reason (I diffed old and
    new after sorting, and they're identical).
    
    There were some actual changes to the OR incentives; they look
    sensible, though I haven't verified them with the primary sources.
    Since OR launch is still a ways off, I'll leave that for a full data
    refresh later.
    
    ## Test Plan
    
    `yarn test`

--- src/data/programs/or_programs.ts
+++ src/data/programs/or_programs.ts
@@ -7,6 +7,14 @@ export const OR_PROGRAMS = {
       en: 'https://www.energytrust.org/residential/incentives',
     },
   },
+  or_energyTrustOfOregon_extendedCapacityHeatPump: {
+    name: {
+      en: 'Extended Capacity Heat Pump',
+    },
+    url: {
+      en: 'https://www.energytrust.org/residential/incentives',
+    },
+  },
   or_energyTrustOfOregon_savingsWithinReach: {
     name: {
       en: 'Savings Within Reach',
@@ -15,36 +23,12 @@ export const OR_PROGRAMS = {
       en: 'https://www.energytrust.org/residential/incentives',
     },
   },
-  or_portlandGeneralElectric_solarForYourHome: {
+  or_pacificPower_extendedCapacityHeatPumpRegionalPromotion: {
     name: {
-      en: 'Solar for your Home',
+      en: 'Extended Capacity Heat Pump Regional Promotion',
     },
     url: {
-      en: 'https://www.energytrust.org/incentives/solar-for-your-home',
-    },
-  },
-  or_portlandGeneralElectric_solarWithinReach: {
-    name: {
-      en: 'Solar Within Reach',
-    },
-    url: {
-      en: 'https://www.energytrust.org/incentives/solar-within-reach',
-    },
-  },
-  or_portlandGeneralElectric_pGESmartCharging: {
-    name: {
-      en: 'PGE Smart Charging',
-    },
-    url: {
-      en: 'https://portlandgeneral.com/energy-choices/electric-vehicles-charging/charging-your-ev/charging-your-ev-at-home',
-    },
-  },
-  or_portlandGeneralElectric_pGESmartThermostatProgram: {
-    name: {
-      en: 'PGE Smart Thermostat Program',
-    },
-    url: {
-      en: 'https://portlandgeneral.com/save-money/save-money-home/smart-thermostat-program',
+      en: 'https://www.energytrust.org/residential/incentives/furnace-and-heat-pump/all',
     },
   },
   or_pacificPower_solarForYourHome: {
@@ -68,7 +52,47 @@ export const OR_PROGRAMS = {
       en: 'Pacific Power Rebates',
     },
     url: {
-      en: 'https://www.pacificpower.net/savings-energy-choices/home.html',
+      en: 'https://www.pacificpower.net/savings-energy-choices/electric-vehicles/home-charger-rebates.html',
+    },
+  },
+  or_pacificPower_heatPumpRegionalPromotion: {
+    name: {
+      en: 'Heat Pump Regional Promotion',
+    },
+    url: {
+      en: 'https://www.energytrust.org/residential/incentives/furnace-and-heat-pump/all',
+    },
+  },
+  or_portlandGeneralElectric_solarForYourHome: {
+    name: {
+      en: 'Solar for your Home',
+    },
+    url: {
+      en: 'https://www.energytrust.org/incentives/solar-for-your-home',
+    },
+  },
+  or_portlandGeneralElectric_solarWithinReach: {
+    name: {
+      en: 'Solar Within Reach',
+    },
+    url: {
+      en: 'https://www.energytrust.org/incentives/solar-within-reach',
+    },
+  },
+  or_portlandGeneralElectric_pGESmartChargingProgram: {
+    name: {
+      en: 'PGE Smart Charging Program',
+    },
+    url: {
+      en: 'https://portlandgeneral.com/energy-choices/electric-vehicles-charging/charging-your-ev/charging-your-ev-at-home',
+    },
+  },
+  or_portlandGeneralElectric_pGESmartThermostatProgram: {
+    name: {
+      en: 'PGE Smart Thermostat Program',
+    },
+    url: {
+      en: 'https://portlandgeneral.com/save-money/save-money-home/smart-thermostat-program',
     },
   },
 } as const;
