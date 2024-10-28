d58f68e78475de346a137a2df23da6daf7e8dcf4
Owen Yamauchi <owen@rewiringamerica.org>
2024-10-28 18:37:15 -0400

    Delete spreadsheet scripts (#602)
    ## Description
    
    At least in theory, we're never importing from spreadsheets into this
    codebase again, so it's time to get rid of all that code.
    
    This also unlocks a bunch of cleanup in the definition of the
    StateIncentive type: we no longer need to maintain separate schemas
    for "collected" and derived incentives.
    
    There's one piece I've left in place:
    `authority-and-program-updater.ts`, which is used by the
    `generate-utility-data` script. I have a separate plan to get rid of
    that, but I want to keep it runnable for now.
    
    Looking at all this, I have mixed feelings. Of course I'm delighted to
    be deleting thousands of lines of code, but I'm a little rueful that
    we spent all that time and effort writing them in the first place, and
    wrangling all those spreadsheets, when we could have been building
    HERO months earlier instead. But hey, it got us through the first wave
    of state launches, so it wasn't a waste.
    
    ## Test Plan
    
    `yarn test`.

--- src/data/state_incentives.ts
+++ src/data/state_incentives.ts
@@ -1,82 +1,38 @@
 import { JSONSchemaType } from 'ajv';
 import fs from 'fs';
-import _ from 'lodash';
-import { AuthorityType } from './authorities';
-import {
-  COLowIncomeAuthority,
-  ILIncomeAuthority,
-  NVLowIncomeAuthority,
-  RILowIncomeAuthority,
-} from './low_income_thresholds';
-import { PROGRAMS } from './programs';
-import { FilingStatus } from './tax_brackets';
 import { AMOUNT_SCHEMA } from './types/amount';
 
 import { START_END_DATE_REGEX } from '../lib/dates';
+import { PROGRAMS } from './programs';
 import { Amount } from './types/amount';
 import { PaymentMethod } from './types/incentive-types';
 import { ALL_ITEMS, Item } from './types/items';
 import { LocalizableString } from './types/localizable-string';
 import { OwnerStatus } from './types/owner-status';
 
-export type LowIncomeAuthority =
-  | 'default'
-  | COLowIncomeAuthority
-  | ILIncomeAuthority
-  | NVLowIncomeAuthority
-  | RILowIncomeAuthority;
-
-// CollectedIncentive and its JSON schema represent the data that lives in raw
-// collected form, e.g. spreadsheets. It should match column-for-column to our
-// spreadsheet format aside from nested fields like amount and column
-// renames/aliases.
-export type CollectedIncentive = {
+export type StateIncentive = {
   id: string;
-  data_urls: string[];
-  authority_type: AuthorityType;
-  authority_name: string;
-  geo_eligibility?: string;
-  program_title: string;
-  program_url: string;
   items: Item[];
-  item_if_selected_other?: string;
   short_description: LocalizableString;
-  program_status: string;
   start_date?: string;
   end_date?: string;
   payment_methods: PaymentMethod[];
-  rebate_value: string;
   amount: Amount;
-  bonus_description?: string;
-  equipment_standards_restrictions?: string;
-  equipment_capacity_restrictions?: string;
-  contractor_restrictions?: string;
-  income_restrictions?: string;
-  filing_status?: FilingStatus;
   owner_status: OwnerStatus[];
-  other_restrictions?: string;
-  stacking_details?: string;
-  financing_details?: string;
-  editorial_notes?: string;
-  questions?: string;
-  omit_from_api?: boolean;
+  eligible_geo_group?: string;
+  program: string;
+  bonus_available?: boolean;
+  low_income?: string;
+  more_info_url?: LocalizableString;
 };
 
-const collectedIncentivePropertySchema = {
+const incentivePropertySchema = {
   id: { type: 'string' },
-  data_urls: { type: 'array', items: { type: 'string' } },
-  authority_type: { type: 'string', enum: Object.values(AuthorityType) },
-  authority_name: { type: 'string' },
-  geo_eligibility: { type: 'string', nullable: true },
-  program_title: { type: 'string' },
-  program_url: { type: 'string' },
   items: {
     type: 'array',
     items: { type: 'string', enum: ALL_ITEMS },
   },
-  item_if_selected_other: { type: 'string', nullable: true },
   short_description: { $ref: 'LocalizableString' },
-  program_status: { type: 'string' },
   start_date: {
     type: 'string',
     pattern: START_END_DATE_REGEX.source,
@@ -92,60 +48,12 @@ const collectedIncentivePropertySchema = {
     items: { type: 'string', enum: Object.values(PaymentMethod) },
     minItems: 1,
   },
-  rebate_value: { type: 'string' },
   amount: AMOUNT_SCHEMA,
-  bonus_description: { type: 'string', nullable: true },
-  equipment_standards_restrictions: { type: 'string', nullable: true },
-  equipment_capacity_restrictions: { type: 'string', nullable: true },
-  contractor_restrictions: { type: 'string', nullable: true },
-  income_restrictions: { type: 'string', nullable: true },
-  filing_status: {
-    type: 'string',
-    enum: Object.values(FilingStatus),
-    nullable: true,
-  },
   owner_status: {
     type: 'array',
     items: { type: 'string', enum: Object.values(OwnerStatus) },
     minItems: 1,
   },
-  other_restrictions: { type: 'string', nullable: true },
-  stacking_details: { type: 'string', nullable: true },
-  financing_details: { type: 'string', nullable: true },
-  editorial_notes: { type: 'string', nullable: true },
-  questions: { type: 'string', nullable: true },
-  omit_from_api: { type: 'boolean', nullable: true },
-} as const;
-const requiredCollectedFields = [
-  'id',
-  'data_urls',
-  'authority_type',
-  'authority_name',
-  'program_title',
-  'program_url',
-  'items',
-  'short_description',
-  'program_status',
-  'payment_methods',
-  'rebate_value',
-  'amount',
-  'owner_status',
-] as const;
-
-// DerivedFields and its schema are associated with data not directly collected
-// in the main spreadsheets. They may have a close relationship with collected
-// fields. Generally, if what's in the spreadsheet requires more than just
-// fitting to an enum or converting from string to number with minor cleanup,
-// it should have a separate collected and derived version.
-export type DerivedFields = {
-  eligible_geo_group?: string;
-  program: string;
-  bonus_available?: boolean;
-  low_income?: LowIncomeAuthority;
-  more_info_url?: LocalizableString;
-};
-
-const derivedIncentivePropertySchema = {
   eligible_geo_group: { type: 'string', nullable: true },
   program: { type: 'string', enum: Object.keys(PROGRAMS) },
   bonus_available: { type: 'boolean', nullable: true },
@@ -155,57 +63,9 @@ const derivedIncentivePropertySchema = {
   },
 } as const;
 
-// Collected fields that pass-through directly to our StateIncentives schema
-// without any modification or processing.
-export const PASS_THROUGH_FIELDS = [
-  'id',
-  'payment_methods',
-  'items',
-  'start_date',
-  'end_date',
-  'amount',
-  'owner_status',
-  'short_description',
-] as const;
-type PassThroughField = (typeof PASS_THROUGH_FIELDS)[number];
-
-const passThroughCollectedProperties = _.pick(
-  collectedIncentivePropertySchema,
-  PASS_THROUGH_FIELDS,
-);
-
-export type StateIncentive = Pick<CollectedIncentive, PassThroughField> &
-  DerivedFields;
-
 export type StateIncentivesMap = {
   [stateId: string]: StateIncentive[];
 };
-
-const incentivePropertySchema = {
-  ...passThroughCollectedProperties,
-  ...derivedIncentivePropertySchema,
-};
-
-// We specify field order which helps when debugging records.
-// This type forces all top-level fields to appear.
-const fieldOrder: {
-  [Key in keyof typeof incentivePropertySchema]: undefined;
-} = {
-  id: undefined,
-  eligible_geo_group: undefined,
-  payment_methods: undefined,
-  items: undefined,
-  program: undefined,
-  amount: undefined,
-  owner_status: undefined,
-  short_description: undefined,
-  start_date: undefined,
-  end_date: undefined,
-  bonus_available: undefined,
-  low_income: undefined,
-  more_info_url: undefined,
-} as const;
-export const FIELD_ORDER = Object.keys(fieldOrder);
 
 const requiredProperties = [
   'id',
@@ -216,14 +76,6 @@ const requiredProperties = [
   'owner_status',
   'short_description',
 ] as const;
-
-export const COLLECTED_DATA_SCHEMA: JSONSchemaType<CollectedIncentive> = {
-  type: 'object',
-  properties: {
-    ...collectedIncentivePropertySchema,
-  },
-  required: requiredCollectedFields,
-} as const;
 
 export const STATE_SCHEMA: JSONSchemaType<StateIncentive> = {
   type: 'object',
