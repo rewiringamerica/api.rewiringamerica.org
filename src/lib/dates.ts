a3a59f2570d25ff4f460590636fd339a28b28a1b
Owen Yamauchi <owen.yamauchi@gmail.com>
2024-12-18 17:46:04 -0500

    Filter out incentives with end dates in the past
    ## Description
    
    Because of the more-than-just-a-day date formats we support, this is a
    little bit subtle.
    
    Note that this does not filter on incentive status, which we are not
    exporting from HERO yet.
    
    Note also that this uses @js-joda, a competent date/time library;
    Javascript's native Date is a nightmare.
    
    ## Test Plan
    
    Unit test of the new date logic. Snapshot tests reveal a bunch of
    expired incentives that don't get returned anymore!

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,3 +1,5 @@
+import { LocalDate, YearMonth } from '@js-joda/core';
+
 /**
  * This allows representing the start or end date of an incentive at the
  * granularity of:
@@ -14,3 +16,42 @@
  */
 export const START_END_DATE_REGEX =
   /^\d{4}(H[12]|Q[1-4]|-(0[1-9]|1[0-2])(-(0[1-9]|1[0-9]|2[0-9]|3[01]))?)?$/;
+
+/**
+ * Returns a LocalDate representing the last possible day of the period denoted
+ * by the given start/end date string. Assumes that the given string matches the
+ * regex above.
+ */
+export function lastDayOf(incentiveDate: string): LocalDate {
+  const quarterHalfMatch = incentiveDate.match(/(\d{4})(Q[1-4]|H[1-2])/);
+  if (quarterHalfMatch) {
+    const year = parseInt(quarterHalfMatch[1]);
+    switch (quarterHalfMatch[2]) {
+      case 'Q1':
+        return LocalDate.of(year, 3, 31);
+      case 'Q2':
+      case 'H1':
+        return LocalDate.of(year, 6, 30);
+      case 'Q3':
+        return LocalDate.of(year, 9, 30);
+      case 'Q4':
+      case 'H2':
+        return LocalDate.of(year, 12, 31);
+    }
+  }
+
+  // Now it must be a normal, partial ISO 8601 date
+  const components = incentiveDate.split('-').map(n => parseInt(n));
+
+  switch (components.length) {
+    case 1:
+      return LocalDate.of(components[0], 12, 31);
+    case 2:
+      return YearMonth.of(components[0], components[1]).atEndOfMonth();
+    case 3:
+      return LocalDate.of(components[0], components[1], components[2]);
+  }
+
+  // Should not get here if START_END_DATE_REGEX precondition holds
+  throw new Error(`Invalid end date ${incentiveDate}`);
+}
