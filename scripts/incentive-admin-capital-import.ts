c6886898595f19a81e4dfd92f0f2c4eb6fcf1ec5
Benj Fredrick <benj@rewiringamerica.org>
2025-03-04 09:07:30 -0700

    Add loan import script

---
+++ scripts/incentive-admin-capital-import.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { writeFileSync } from 'fs';
+
+const INCENTIVE_ADMIN_HOST =
+  process.env.INCENTIVE_ADMIN_HOST || 'http://localhost:3000/api/export';
+
+const GENERAL_IMPORTS = ['/loan-programs?status=PUBLISHED'];
+
+function pathToFileName(path: string) {
+  // Remove query params and slashes; replace dashes with underscores
+  return path.split('?')[0].replaceAll('/', '').replaceAll('-', '_') + '.json';
+}
+
+async function saveLoanProgramDataToDisk() {
+  for (const path of GENERAL_IMPORTS) {
+    const writeDirAndFilename = `data/${pathToFileName(path)}`;
+    const data = await fetchCapitalData(path);
+    writeFileSync(writeDirAndFilename, JSON.stringify(data, null, 2) + '\n');
+    console.log(`Exported ${writeDirAndFilename}...`);
+  }
+}
+
+async function fetchCapitalData(path: string) {
+  const data_url = `${INCENTIVE_ADMIN_HOST}${path}`;
+  const authHeader = process.env.CAPITAL_API_KEY;
+  console.log(`fetching data from ${data_url}`);
+
+  try {
+    const response = await fetch(data_url, {
+      headers: {
+        Authorization: `${authHeader}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Error fetching data: ${response.status} ${response.statusText}`,
+      );
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Failed to fetch data from ${data_url}:`, error);
+    throw error;
+  }
+}
+
+async function main() {
+  console.log('Running incentive-admin-capital-import.ts');
+  await saveLoanProgramDataToDisk();
+  console.log('Finished running incentive-admin-capital-import.ts');
+}
+
+main();
